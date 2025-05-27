83d200556a40eca42c10eee0661f8606edeb03db
AntonioAldamaG <22041328@itdurango.edu.mx>
2025-05-27 17:12:01 -0600

    a

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -3,10 +3,15 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { LoginEventosSahuaroComponent } from "../login-eventos-sahuaro/login-eventos-sahuaro.component";
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { CostComponent } from '../cost/cost.component';
+import { DatesComponent } from '../dates/dates.component';
+import { MediaComponent } from '../media/media.component';
+import { StockComponent } from '../stock/stock.component';
 
 @Component({
   selector: 'app-main-page',
-  imports: [RouterOutlet, RouterModule, CommonModule, LoginEventosSahuaroComponent, NavBarComponent],
+  imports: [RouterOutlet, RouterModule, CommonModule, LoginEventosSahuaroComponent, NavBarComponent, CostComponent,
+    DatesComponent, MediaComponent, StockComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
