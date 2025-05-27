aa44558cbb21d98be3a41bcfb0fbeb6d9e63be5b
AntonioAldamaG <22041328@itdurango.edu.mx>
2025-05-27 16:16:35 -0600

    a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MediaComponent } from './components/media/media.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { LoginEventosSahuaroComponent } from './components/login-eventos-sahuaro/login-eventos-sahuaro.component';
 
 export const routes: Routes = [
-    { path: '', component: MainPageComponent, title: 'main' },
+    { path: '', component: LoginEventosSahuaroComponent, title: 'main' },
+    {path: 'app-main-page', component: MainPageComponent, title: 'main'},
     { path: 'app-media', component: MediaComponent, title: 'media' },
 ];
